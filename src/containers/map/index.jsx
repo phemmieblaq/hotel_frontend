759b090349c112ece781d197f78b56a148572087
phemmieblaq <femiadeyemo008@gmail.com>
2023-11-15 23:44:22 +0000

    google map added

--- src/containers/map/index.jsx
+++ src/containers/map/index.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '60vw',
+  height: '50vh',
+};
+const center = {
+  lat:52.6221, // East Anglia latitude
+  lng: 1.2411, // East Anglia longitude
+};
+
+const MapContainer = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCeITd4_ynvruPcpnS8GTC14a-WJI7zIwo',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
   return (
     <div>
-      
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default MapContainer;
