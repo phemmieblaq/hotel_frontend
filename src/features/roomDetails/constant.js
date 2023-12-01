7adbe69826430718db97da8b9c089f9699f82ada
phemmieblaq <femiadeyemo008@gmail.com>
2023-12-01 15:26:48 +0000

    all codes in a folder

--- src/features/roomDetails/constant.js
+++
@@ -1,7 +0,0 @@
-import suite from '../../assets/png/suite.jpeg'
-import gym from '../../assets/png/gym.jpeg'
-import hall from '../../assets/png/hall.jpeg'
-import park from '../../assets/png/park.png'
-
-
-// export const deluxeImages =[ suite, hall, park]
